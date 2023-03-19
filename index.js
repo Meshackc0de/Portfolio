e7509d56d1da041a3f1b2c1fbef48198694f0818
Meshackc0de <113516903+Meshackc0de@users.noreply.github.com>
2023-03-19 11:21:29 +0300

    My portfolio

---
+++ index.js
@@ -0,0 +1,6 @@
+const contactButton = document.querySelector("#contact-btn");
+const contactSection = document.querySelector("#contact-sectn");
+
+contactButton.addEventListener('click', () => {
+    contactSection.style.display = 'block';
+  });
